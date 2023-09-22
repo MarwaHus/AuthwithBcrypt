d7e5d9a7ff684185f58d1570e41912af3b2b64ef
MarwaHus <marwa.hu18@gmail.com>
2023-09-23 00:32:05 +0300

    lab14

--- src/main/java/com/Signup/authWithBcrypt/controller/AuthPost.java
+++ src/main/java/com/Signup/authWithBcrypt/controller/AuthPost.java
@@ -1,36 +1,43 @@
 package com.Signup.authWithBcrypt.controller;
+import com.Signup.authWithBcrypt.models.PostUser;
 import com.Signup.authWithBcrypt.models.SiteUser;
+import com.Signup.authWithBcrypt.repositories.PostRepository;
 import com.Signup.authWithBcrypt.repositories.UserRepository;
-import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
-
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class AuthPost {
     @Autowired
     UserRepository userRepository;
 
-    @PostMapping("/post")
-    public RedirectView logInUSerWithSecret(HttpServletRequest request, String username, String password){
-        SiteUser userDataBase=userRepository.findByUsername(username);
-
-        if((userDataBase == null)
-                || (!BCrypt.checkpw(password, userDataBase.getPassword())))
-        {
-
-            return new RedirectView("/post");
+    @Autowired
+    PostRepository postRepository;
+    @GetMapping("/post")
+    public String getHomePage(Model model, HttpServletRequest request){
+        if(request.getSession().getAttribute("username") != null){
+            List<PostUser> posts = postRepository.findAll();
+            model.addAttribute("posts", posts);
+            String username = (String) request.getSession().getAttribute("username");
+            model.addAttribute("username", username);
+            return "loginpost";
         }
-        HttpSession session= request.getSession();
-        session.setAttribute("username", username);
-
-        return new RedirectView("/login");
+        return "redirect:/login";
     }
-
+    @PostMapping("/posts")
+    public RedirectView savePost(HttpServletRequest request, String textContent){
+        String username = (String) request.getSession().getAttribute("username");
+        SiteUser user = userRepository.findByUsername(username);
+        PostUser post = new PostUser(textContent, user);
+        postRepository.save(post);
+        return new RedirectView("/post");
+    }
 
 }
 
